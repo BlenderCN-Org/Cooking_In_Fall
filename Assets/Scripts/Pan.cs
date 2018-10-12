396f36331269a9bd687163529b2d29951d1b5999
Sebastien Gaumin <sgaumin@gmail.com>
2018-10-13 01:46:46 +0200

    Second Physics Tests

---
+++ Assets/Scripts/Pan.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pan : MonoBehaviour {
+
+    public Text score;
+
+    private int currentScore;
+
+	// Use this for initialization
+	void Start () {
+        currentScore = 0;
+        score.text = currentScore.ToString("0");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Fruits"))
+        {
+            Destroy(other.gameObject);
+            currentScore++;
+            score.text = currentScore.ToString("0");
+        }
+    }
+}
