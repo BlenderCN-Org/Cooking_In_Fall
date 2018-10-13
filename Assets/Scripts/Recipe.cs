ab5f9adf01ba5ecae76a0d149e7ef0a7aa5a2f7b
tarazhong <zhong.tara@gmail.com>
2018-10-13 02:18:03 +0200

    Added Recipe Canvas and Scripts

---
+++ Assets/Scripts/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Generates UI for recipe
+// Ex : Tomato soup = 2 tomatoes
+// Tomato soup
+// 2 Tomato
+public class Recipe : MonoBehaviour {
+    public string recipeName;
+    public Ingredients[] recipe;
+
+    public Transform parentIngredients;
+    public GameObject prefabIngredient;
+
+    private void Start()
+    {
+        // Init UI
+        this.gameObject.transform.Find("Name").GetComponent<Text>().text = recipeName; // Replace recipe name
+        foreach (Ingredients ing in recipe) // Create ingredients
+        {
+            GameObject newIng = Instantiate(prefabIngredient,parentIngredients);
+            newIng.transform.GetChild(0).GetComponent<Text>().text = ing.number + " : "+ ing.type;
+        }
+
+    }
+}
